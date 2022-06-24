dd484cb33c89aa3c63ece8fbd1c1445877e95bcc
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-06-24 13:46:11 +0530

    Update TestRun.java

--- src/test/java/BaseClass/TestRun.java
+++ src/test/java/BaseClass/TestRun.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "." }, glue = { "stepDefination" }, tags = ("@FreeLogin"),
+@CucumberOptions(features = { "." }, glue = { "stepDefination" }, tags = ("@EbooksGoogle"),
 plugin = { "pretty","html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json", "usage:target/usage.jsonx",
 		"junit:target/cucumber.xml" }
 
