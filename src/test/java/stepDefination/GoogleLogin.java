c27faa7d6b3249c8f0d76060c4459397374938a4
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-06-24 13:38:51 +0530

    Update GoogleLogin.java

--- src/test/java/stepDefination/GoogleLogin.java
+++ src/test/java/stepDefination/GoogleLogin.java
@@ -89,7 +89,7 @@ public class GoogleLogin extends SetUPClass {
 
 				WebElement g_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
 						"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")));
-				g_email.sendKeys("nisha.dhiman@slidetech.in");
+				g_email.sendKeys("slidetech.qa@gmail.com");
 				Thread.sleep(3000);
 
 				WebElement g_login_btn1 = wait.until(ExpectedConditions
@@ -101,7 +101,7 @@ public class GoogleLogin extends SetUPClass {
 				WebElement g_pass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
 						"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")));
 
-				g_pass.sendKeys("Nisha@123");
+				g_pass.sendKeys("Himanshi@123");
 
 				Thread.sleep(3000);
 				WebElement g_login_btn2 = wait.until(
