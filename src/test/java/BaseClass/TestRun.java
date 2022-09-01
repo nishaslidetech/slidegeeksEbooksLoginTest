033b4c58597d9d1f3bdf3878351130a6d1240a3a
nishaslidetech <nisha.dhiman@slidetech.in>
2022-09-01 15:13:26 +0530

    update browser

--- src/test/java/BaseClass/TestRun.java
+++ src/test/java/BaseClass/TestRun.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = { "." }, glue = { "stepDefination" }, tags = ("@EbooksGoogle"),
+@CucumberOptions(features = { "." }, glue = { "stepDefination" },// tags = ("@EbooksGoogle"),
 
 		plugin = { "pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json",
 				"usage:target/usage.jsonx", "junit:target/cucumber.xml" }
