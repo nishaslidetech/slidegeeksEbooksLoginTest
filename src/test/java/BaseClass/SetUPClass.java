c3c5ddc7e6aa0a1c99525cc7604faa9967ad701f
nishaslidetech <nisha.dhiman@slidetech.in>
2022-09-01 18:05:58 +0530

    update browser

--- src/test/java/BaseClass/SetUPClass.java
+++ src/test/java/BaseClass/SetUPClass.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -28,11 +29,9 @@ public class SetUPClass {
 	public static WebDriver driver;
 	public static String AppURL;
 	public static Properties property = new Properties(System.getProperties());
-	public static String browserName;
+	public static String browser;
 	public static Logger log;
-	public static WebElement webelement;
-	public static String local_chrome;
-	public static String local_FFbrowser;
+
 	public static WebDriverWait wait;
 	protected String actualUrl = "https://www.slidegeeks.com/";
 	public static JavascriptExecutor js;
@@ -40,49 +39,63 @@ public class SetUPClass {
 	@BeforeClass
 	public static void before_Class() throws Exception {
 		log = Logger.getLogger(BeforeClass.class.getName());
-		property.load(new FileReader("config//config.properties"));
+		property.load(new FileReader("Config//config.properties"));
 		AppURL = property.getProperty("App_url");
-		local_chrome = property.getProperty("local_chrome");
-		local_FFbrowser = property.getProperty("local_FFbrowser");
-		// on source lab setup
-		AppURL = property.getProperty("App_url");
+
 		System.out.println("Bname=====" + AppURL);
 
-		if ((local_chrome.equals("yes"))) {
+		if (System.getenv("browser") != null && !System.getenv("browser").isEmpty()) {
+			browser = System.getenv("browser");
+			System.out.println("env browser = " + browser);
+
+		} else {
+			browser = property.getProperty("browser");
+			System.out.println("config browser = " + browser);
+		}
+
+		property.setProperty("browser", browser);
+
+		System.out.println("setProperty browser = " + browser);
+
+		if ((property.getProperty("browser").equals("chrome"))) {
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--disable-notifications");
-			// options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-			/*
-			 * options.addArguments("--incognito"); // DesiredCapabilities object
-			 * DesiredCapabilities c = DesiredCapabilities.chrome(); // set capability to
-			 * c.setCapability(ChromeOptions.CAPABILITY, options);
-			 */
-
 			driver = new ChromeDriver(options);
-
 			driver.manage().window().maximize();
-
-			// driver.get(AppURL);
-			driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
-			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-			wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait = new WebDriverWait(driver, 50);
 			js = (JavascriptExecutor) driver;
+			Thread.sleep(1000);
 		}
 		// if (browser.equalsIgnoreCase("firefox"))
 
 		// if (browser.equalsIgnoreCase("chrome"))
-		else if ((local_FFbrowser.equals("yes"))) {
+		else if ((property.getProperty("browser").equals("firefox"))) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 			driver.manage().window().maximize();
-			driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
-			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-			wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-			js = (JavascriptExecutor) driver;
+			driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
+			driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+			wait = new WebDriverWait(driver, 50);
+			js = (JavascriptExecutor) driver;
 			Thread.sleep(1000);
-		} else {
+		} else if ((property.getProperty("browser").equals("edge"))) {
+
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+			driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+			wait = new WebDriverWait(driver, 50);
+			js = (JavascriptExecutor) driver;
+			Thread.sleep(1000);
+		}
+
+		else {
 
 			System.out.println("platform does not provide");
 		}
